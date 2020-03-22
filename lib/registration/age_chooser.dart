71bb8447c47c69f4a05030d673d7f99326954f8d
Christian Epping <christian.epping@mni.thm.de>
2020-03-22 14:19:32 +0100

    Forms

---
+++ lib/registration/age_chooser.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:intl/intl.dart';
+
+class AgeChooser extends StatefulWidget {
+  AgeChooser({Key key}) : super(key: key);
+
+  @override
+  _AgeChooserState createState() => _AgeChooserState();
+}
+
+class _AgeChooserState extends State<AgeChooser> {
+  final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('COVID Hub'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Wähle dein Alter',
+                style: TextStyle(fontSize: 32),
+              ),
+              FormBuilder(
+                key: _fbKey,
+                autovalidate: true,
+                child: Column(
+                  children: <Widget>[
+                    FormBuilderTextField(
+                      attribute: 'year',
+                      maxLines: 1,
+                      decoration: const InputDecoration(labelText: 'Jahrgang'),
+                      validators: [
+                        FormBuilderValidators.required(),
+                        FormBuilderValidators.minLength(4,
+                            errorText: 'Bitte im Format "JJJJ" angeben'),
+                        FormBuilderValidators.maxLength(4,
+                            errorText: 'Bitte im Format "JJJJ" angeben'),
+                        FormBuilderValidators.numeric(
+                            errorText: 'Bitte im Format "JJJJ" angeben'),
+                        FormBuilderValidators.min(1900,
+                            errorText: 'Sie sind ein medizinisches Wunder'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                children: <Widget>[
+                  MaterialButton(
+                    color: Colors.teal,
+                    textColor: Colors.white,
+                    child: Text("Submit"),
+                    onPressed: () {
+                      if (_fbKey.currentState.saveAndValidate()) {
+                        print(_fbKey.currentState.value);
+                        /*Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (context) => AgeChooser(_fbKey),
+                          ),
+                        );*/
+                      }
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
